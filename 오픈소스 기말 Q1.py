ef609add1be48c7cc7e92dfe9c20c42d46412eb3
choiseongpil <152938204+choiseongpil@users.noreply.github.com>
2023-12-11 19:20:35 +0900

    Update 오픈소스 기말 Q1.py

--- 오픈소스 기말 Q1.py
+++ 오픈소스 기말 Q1.py
@@ -1,7 +1,8 @@
 def solution(my_string, target):
   answer = 0
-  
-  if 1 <= len(my_string) <=100 and 1<= len(target) <=100 and my_string.islower() and target.islower():   #제한조건을 만족하기 위해서 넣음 앞부분은 이하제한, 뒷부분은 소문자 판별
+
+  #제한조건을 만족하기 위해서 넣음 앞부분은 이하제한, 뒷부분은 소문자 판별
+  if 1 <= len(my_string) <=100 and 1<= len(target) <=100 and my_string.islower() and target.islower():   
     if target in my_string:          #if in 함수를 사용해서 B안에 A의 문자열이 있는지 따짐
       answer = 1                  #만약 존재하면 1 아니면 0 반환
       
