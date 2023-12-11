0d6542dd99cb1eb440a07685658ece390f43a2b3
choiseongpil <152938204+choiseongpil@users.noreply.github.com>
2023-12-11 19:30:37 +0900

    Update 오픈소스 기말 Q5.py

--- 오픈소스 기말 Q5.py
+++ 오픈소스 기말 Q5.py
@@ -2,7 +2,8 @@ def solution(numbers):
     if len(numbers) > 1000000:           # numbers의 길이를 1000000으로 제한 넘으면 오류 발생
         raise ValueError("Error: The length of 'numbers' exceeds the limit of 1000000")
 
-    numbers = [str(i)[:1000] for i in numbers]      # 각 원소의 길이를 1000으로 제한하고 문자열로 변환이 목적이지만 현재 코드를 돌려보면 왜인지 오류발생이 안뜸
+    s]      # 각 원소의 길이를 1000으로 제한하고 문자열로 변환이 목적이지만 현재 코드를 돌려보면 왜인지 오류발
+    numbers = [str(i)[:1000] for i in numbers]  
     for num in numbers:
         if len(num) > 1000:
             raise ValueError(f"Error: The length of an element ({num}) exceeds the limit of 1000")
