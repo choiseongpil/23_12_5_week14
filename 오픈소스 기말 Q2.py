a4ea5b54034d0f11aa1ae9df4d085ef4628ac1f2
choiseongpil <152938204+choiseongpil@users.noreply.github.com>
2023-12-11 19:06:48 +0900

    Update 오픈소스 기말 Q2.py

--- 오픈소스 기말 Q2.py
+++ 오픈소스 기말 Q2.py
@@ -1,24 +1,22 @@
 def solution(letter):
-  morse = { 
+    morse = { 
     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
     '-.--':'y','--..':'z'}
-  
-  answer = ''
-  
-  if 1 <= len(letter) <= 1000:              #제한조건을 만족하기위함
-    words = letter.split(' ')               #공백으로 나누어져 있으니 스플릿을 이용해 나누고 해독하여 영어로 변환
-    for word in words:
-      if word:                               #연속된 공백이 아니면 처리
-        if word in morse:                    #해당 모스부호가 존재하지 않으면 공백처리
-          answer += morse[word]
-        else:
-          answer += ' '
-  return answer
+    
+    answer = ''
+    
+    # 제한사항에 따라 길이 확인
+    if 1 <= len(letter) <= 1000: # 공백을 기준으로 Morse 부호를 분리하고 해독하여 영어 소문자로 변환
+        words = letter.split(' ')
+        for word in words:
+            answer += morse.get(word, ' ')  # 해당 Morse 부호가 없으면 공백을 추가
+    
+    return answer
 
-#예시로 든 것
-letter = ".... . .-.. .-.. --- .-- --- .-. .-.. -.."       #이것을 돌려보면 hello world를 얻을 수 있음
+# 예제 테스트
+letter = ".... . .-.. ---  .-- --- .-. .-.. -.."
 answer = solution(letter)
-print(answer)
+print(result)  # 예상 결과: hello world
